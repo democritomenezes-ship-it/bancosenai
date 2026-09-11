184bccd5ca00b3bf648e76b84e9a4885751365dc
democritomenezes-ship-it <democrito.menezes@aluno.senai.br>
2026-09-11 08:43:39 -0300

    feat(documentos): criacao de controller e model de documentos

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadado.cs
@@ -0,0 +1,11 @@
+﻿namespace BancoSENAIAPI.Models
+{
+    public class DocumentoMetadado
+    {
+        public int id { get; set; }
+        public string name { get; set; }    
+        public string Extensao { get; set;}
+        public string Caminho { get; set;}
+        public int CodigoCliente { get; set;}
+    }
+}
