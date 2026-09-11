a0e37026d2ec4d6c92ada7a158a2ac80260d838c
democritomenezes-ship-it <democrito.menezes@aluno.senai.br>
2026-09-11 08:50:54 -0300

    feat(documentos) inicio upload endpoint

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -14,6 +14,10 @@ public class DocumentoController : Controller
         private static List<Models.DocumentoMetadado> _documentosMetadados = new List<Models.DocumentoMetadado>();
 
         private static int _nextId = 1;
+        [HttpPost("upload/{codigoCliente}")]
+        public async Task<ActionResult> AnexarArquivo(int CodigoCliente, IFormFile arquivo)
+        {
 
+        }
     }
 }
